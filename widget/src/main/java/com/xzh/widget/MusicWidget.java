2a8d04046da7cae5b981c20de63a370e8234233b
31702160323 <13005687228@163.com>
2021-02-27 15:12:58 +0800

    提交测试版本

--- widget/src/main/java/com/xzh/widget/MusicWidget.java
+++ widget/src/main/java/com/xzh/widget/MusicWidget.java
@@ -1,26 +1,52 @@
-package com.xzh.musicnotification.view;
+package com.xzh.widget;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
 import android.util.Log;
 import android.widget.RemoteViews;
-
-import com.xzh.musicnotification.R;
-import com.xzh.musicnotification.utils.PendingIntentInfo;
 
 /**
  * Implementation of App Widget functionality.
  */
 public class MusicWidget extends AppWidgetProvider {
 
+    class PendingIntentInfo {
+        private int Id;
+        private int Index;
+
+        public PendingIntentInfo(int id, int index){
+            this.Id = id;
+            this.Index = index;
+        }
+
+        public int getId() {
+            return Id;
+        }
+
+        public int getIndex() {
+            return Index;
+        }
+    }
+
+    public void openAppIntent(RemoteViews views, Context context, PendingIntentInfo... pendingIntentInfoList){
+        for (PendingIntentInfo item: pendingIntentInfoList) {
+            Intent intent = new Intent("io.dcloud.PandoraEntry");
+            intent.setClassName(context, "io.dcloud.PandoraEntryActivity");
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            views.setOnClickPendingIntent(item.getId(), PendingIntent.getActivity(context, item.getIndex(), intent, PendingIntent.FLAG_UPDATE_CURRENT));
+        }
+    }
+
     private void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
-                                int appWidgetId) {
+                                 int appWidgetId) {
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.music_widget);
 
         //打开应用
-        PendingIntentInfo.openAppIntent(views, context,
+        this.openAppIntent(views, context,
                 new PendingIntentInfo(R.id.image_view, 0),
                 new PendingIntentInfo(R.id.play_view, 1),
                 new PendingIntentInfo(R.id.previous_view, 2),
@@ -30,6 +56,11 @@ public class MusicWidget extends AppWidgetProvider {
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
     }
 
     @Override
