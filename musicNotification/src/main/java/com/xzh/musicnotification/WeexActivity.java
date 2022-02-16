f2082e7291b25fa7e686f3d849c0a8cb9fc6f4c6
xzh <13005687228@163.com>
2022-02-17 00:39:29 +0800

    fefactor: 重构ServiceBinder

---
+++ musicNotification/src/main/java/com/xzh/musicnotification/WeexActivity.java
@@ -0,0 +1,85 @@
+package com.xzh.musicnotification;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.taobao.weex.IWXRenderListener;
+import com.taobao.weex.WXSDKEngine;
+import com.taobao.weex.WXSDKInstance;
+import com.taobao.weex.common.WXException;
+import com.taobao.weex.common.WXRenderStrategy;
+import com.taobao.weex.utils.WXFileUtils;
+import com.xzh.musicnotification.utils.Utils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class WeexActivity extends AppCompatActivity implements IWXRenderListener {
+    WXSDKInstance mWXSDKInstance;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD | WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1) {
+            setShowWhenLocked(true);
+        }
+        Utils.fullScreen(this);
+
+        mWXSDKInstance = new WXSDKInstance(this);
+        mWXSDKInstance.registerRenderListener(this);
+        Map<String, Object> options = new HashMap<>();
+        options.put(WXSDKInstance.BUNDLE_URL, "source");
+        mWXSDKInstance.render(getPackageName(), WXFileUtils.loadAsset("build/index.js", this), options, null, WXRenderStrategy.APPEND_ASYNC);
+    }
+    @Override
+    public void onViewCreated(WXSDKInstance instance, View view) {
+        Log.d("TAG", "onViewCreated: " + view);
+        setContentView(view);
+    }
+    @Override
+    public void onRenderSuccess(WXSDKInstance instance, int width, int height) {
+    }
+    @Override
+    public void onRefreshSuccess(WXSDKInstance instance, int width, int height) {
+    }
+
+    @Override
+    public void onException(WXSDKInstance instance, String errCode, String msg) {
+        Log.d("TAG", "onException: " + errCode);
+        Log.d("TAG", "onException: " + msg);
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(mWXSDKInstance!=null){
+            mWXSDKInstance.onActivityResume();
+        }
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(mWXSDKInstance!=null){
+            mWXSDKInstance.onActivityPause();
+        }
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(mWXSDKInstance!=null){
+            mWXSDKInstance.onActivityStop();
+        }
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(mWXSDKInstance!=null){
+            mWXSDKInstance.onActivityDestroy();
+        }
+    }
+}
