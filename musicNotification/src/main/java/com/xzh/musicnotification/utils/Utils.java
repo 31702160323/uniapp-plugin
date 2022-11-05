1a5ea203131c2ba1631ce0f26e5b689731d37032
xzh <13005687228@163.com>
2022-11-05 17:03:49 +0800

    提交代码

--- musicNotification/src/main/java/com/xzh/musicnotification/utils/Utils.java
+++ musicNotification/src/main/java/com/xzh/musicnotification/utils/Utils.java
@@ -3,6 +3,7 @@ package com.xzh.musicnotification.utils;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.PendingIntent;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
@@ -107,5 +108,57 @@ public class Utils {
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
         window.setStatusBarColor(Color.TRANSPARENT);
 
+    }
+
+    /**
+     * 获取自启动管理页面的Intent
+     *
+     * @param context context
+     * @return 返回自启动管理页面的Intent
+     */
+    public static Intent getAutostartSettingIntent(Context context) {
+        ComponentName componentName = null;
+        String brand = Build.MANUFACTURER;
+        Intent intent = new Intent();
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        switch (brand.toLowerCase()) {
+            case "samsung"://三星
+                componentName = new ComponentName("com.samsung.android.sm", "com.samsung.android.sm.app.dashboard.SmartManagerDashBoardActivity");
+                break;
+            case "huawei"://华为
+                componentName = new ComponentName("com.huawei.systemmanager", "com.huawei.systemmanager.appcontrol.activity.StartupAppControlActivity");
+//            componentName = new ComponentName("com.huawei.systemmanager", "com.huawei.systemmanager.startupmgr.ui.StartupNormalAppListActivity");//目前看是通用的
+                break;
+            case "xiaomi"://小米
+                componentName = new ComponentName("com.miui.securitycenter", "com.miui.permcenter.autostart.AutoStartManagementActivity");
+//                componentName = new ComponentName("com.android.settings","com.android.settings.BackgroundApplicationsManager");
+                break;
+            case "vivo"://VIVO
+//            componentName = new ComponentName("com.iqoo.secure", "com.iqoo.secure.safaguard.PurviewTabActivity");
+                componentName = new ComponentName("com.iqoo.secure", "com.iqoo.secure.ui.phoneoptimize.AddWhiteListActivity");
+                break;
+            case "oppo"://OPPO
+//            componentName = new ComponentName("com.oppo.safe", "com.oppo.safe.permission.startup.StartupAppListActivity");
+                componentName = new ComponentName("com.coloros.oppoguardelf", "com.coloros.powermanager.fuelgaue.PowerUsageModelActivity");
+                break;
+            case "yulong":
+            case "360"://360
+                componentName = new ComponentName("com.yulong.android.coolsafe", "com.yulong.android.coolsafe.ui.activity.autorun.AutoRunListActivity");
+                break;
+            case "meizu"://魅族
+                componentName = new ComponentName("com.meizu.safe", "com.meizu.safe.permission.SmartBGActivity");
+                break;
+            case "oneplus"://一加
+                componentName = new ComponentName("com.oneplus.security", "com.oneplus.security.chainlaunch.view.ChainLaunchAppListActivity");
+                break;
+            case "letv"://乐视
+                intent.setAction("com.letv.android.permissionautoboot");
+            default://其他
+                intent.setAction("android.settings.APPLICATION_DETAILS_SETTINGS");
+                intent.setData(Uri.fromParts("package", context.getPackageName(), null));
+                break;
+        }
+        intent.setComponent(componentName);
+        return intent;
     }
 }
